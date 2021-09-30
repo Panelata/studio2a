17aedaa4eb701445425d8154c006ff92a194d73d
Andy <andy.le314@gmail.com>
2021-10-01 08:23:40 +1000

    added login page errors and reveal password

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -6,6 +6,7 @@ import './loginStyle.css'
 const Login = props => {
 	const [username, setUsername] = React.useState('');
 	const [password, setPassword] = React.useState('');
+	const [shown, setShown] = React.useState(false);
 
 	const login = (ev) =>{
 		ev.preventDefault();
@@ -16,8 +17,18 @@ const Login = props => {
 				props.checkLogin();
 			} else {
 				console.log("Unsuccessful");
+				document.getElementById("error").textContent = "Inncorrect username or password."
 			}
 		});
+	}
+
+	const toggleVisiblity = () => {
+		var test = document.getElementById("reveal")
+		if(shown)
+			test.textContent = "Reveal password"
+		else
+			test.textContent = "Hide password"
+		setShown(shown ? false : true);
 	}
 
 	return (
@@ -27,7 +38,9 @@ const Login = props => {
 				<label>Username</label>
 				<input required type="text" onChange={(ev)=>setUsername(ev.target.value)} />
 				<label>Password</label>
-				<input required type="password" onChange={(ev)=>setPassword(ev.target.value)} />
+				<input required type={shown ? "text" : "password"} onChange={(ev)=>setPassword(ev.target.value)} />
+				<div class="inner"> <i id="reveal" role="button" onClick={toggleVisiblity} title={shown ? "Hide password" : "Reveal password"}> Reveal password </i> </div>
+				<span id="error" className="error"> </span>
 				<button type="submit" className="btn btn-primary btn-block btn-large">Log In</button>
 			</form>
 		</div>
