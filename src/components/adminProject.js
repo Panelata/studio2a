b9b0089b744bf12d778ab5383e756bcc2d99f267
Dinh Luu <12748556@student.uts.edu.au>
2021-10-18 21:48:24 +1100

    Add skills dropdown and removal

--- src/components/adminProject.js
+++ src/components/adminProject.js
@@ -2,11 +2,20 @@ import React from 'react';
  const AdminProject = (props) => {
     const [projectName, setProjectName] = React.useState('');
     const [size, setSize] = React.useState('');
+    const [skills, setSkills] = React.useState([]);
     
     React.useEffect(()=>{
         console.log(props);
     },[]);
 
+    const addSkill = (value) =>{
+        setSkills(skills => [...skills, value]);
+    }
+
+    const removeSkill = (skill) => {
+        console.log('removing ' + skill);
+        setSkills(skills.filter(item => item !== skill));
+    }
 
      return (
          <div>
@@ -15,6 +24,22 @@ import React from 'react';
             <input type="text" onChange={(ev)=>setProjectName(ev.target.value)} />
             <label>Group Size</label>
             <input type="number" onChange={(ev)=>setSize(ev.target.value)} />
+            {
+                skills.map((skill, key) => 
+                    <div key={key}>
+                      <p>{skill}</p>
+                      <button onClick={()=>{removeSkill(skill)}}>Remove</button>
+                    </div>
+                )
+            }
+            <label>Add Skill</label>
+            <select onChange={(ev)=>addSkill(ev.target.value)}>
+                    <option hidden>Select Skill</option>
+                    <option>Skill 1</option>
+                    <option>Skill 2</option>
+                    <option>Skill 3</option>
+                    <option>Skill 4</option>
+            </select> 
          </div>
      )
  }
