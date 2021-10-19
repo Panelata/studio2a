eba972d155a100ae8f8269c6f84e8836e0e5a052
Tonyhanna9 <Antonious.hanna@student.uts.edu.au>
2021-10-20 02:08:40 +1100

    Student Home Page Syling

--- src/components/studentHome.js
+++ src/components/studentHome.js
@@ -1,39 +1,88 @@
 import React from 'react';
 import styles from "./studentHome.module.css"
+import "./studentHomeStyles.css"
 import { Link } from 'react-router-dom';
 
 // example class list for demo before db connection
-const classes = [
+const allClasses = [
 	{
-		id: "01231234",
-		name: "Software Engineering Studio 1a"
+		classId: 1,
+		className: "Autumn 2021",
+		classSubjects: [
+			{
+				id: "68037",
+				name: "Physical Modelling"
+			},
+			{
+				id: "33130",
+				name: "Mathematical Modelling 1"
+			},
+			{
+				id: "26100",
+				name: "Integrating Business Perspectives"
+			},
+			{
+				id: "48230",
+				name: "Engineering Communication"
+			},
+		]
 	},
 	{
-		id: "89786231",
-		name: "Data Structures and Algorithms"
+		classId: 2,
+		className: "Spring 2021",
+		classSubjects: [
+			{
+				id: "01231234",
+				name: "Software Engineering Studio 1A"
+			},
+			{
+				id: "48240",
+				name: "Design and Innovation Fundamentals"
+			},
+			{
+				id: "33230",
+				name: "Mathematical Modelling 2"
+			},
+			{
+				id: "48023",
+				name: "Programming Fundamentals	"
+			},
+		]
 	},
-	{
-		id: "89712121",
-		name: "Engineering Communication"
-	}
 ];
 
 const StudentHome = () => {
 	return (
-		<div>
-			<h1>student homepage </h1>
-			<div className={styles.mainContainer}>This is the student box
-				{classes ? (classes.map(studentClass => 
-					<div key={studentClass.id}>
-					<Link to={{
-						pathname: "/student/class",
-						subjectName: studentClass.name
-					}}
-					>View {studentClass.name}!</Link>
-					</div>
-				)) :
-					(<div>Sorry you haven't joined/been added to any classes yet</div>)
-				}
+		<div className="group-arrangement-pg-wrapper">
+			<div className="group-arrangement-pg-container">
+				<div className="page-heading-container">
+					<h1 className="page-heading">Group Arrangement</h1>
+				</div>
+				<div className={`${styles.mainContainer} main-data-container`}>
+					<h3 className="all-groups-single-head">Group arrangements for subjects below: </h3>
+					{allClasses.length > 0 ? allClasses.map((classItem, index) => {
+						return (
+							<div className="group-head-and-group-row-container">
+								<h3 className="group-head-name">{classItem.className}</h3>
+								<div className="subjects-group-container">
+									{classItem.classSubjects.map(studentClass =>
+										<div key={studentClass.id} className="subject-container">
+											<Link to={{
+												pathname: "/student/class",
+												subjectName: studentClass.name
+											}}
+											>{studentClass.name}</Link>
+										</div>
+									)}
+								</div>
+							</div>
+						);
+					})
+						:
+						(<div className="error-no-joining">Sorry you haven't joined/been added to any classes yet</div>)
+					}
+
+				</div>
 			</div>
 		</div>
 	)
