9830302f96c8d7c7cc9764cd9dbdb1a771512977
codyfp <fisherpeel.c@gmail.com>
2021-10-21 20:39:02 +1100

    small data/spelling fixes

--- src/components/adminManageGroups.js
+++ src/components/adminManageGroups.js
@@ -2,36 +2,38 @@ import React, { useState } from 'react';
 import styles from "./adminManageGroups.module.css";
 import kmeans from "node-kmeans";
 
+
+
 const data = [
-	{ 'name': 'Tobias Snyder', 'skill-a': 100, 'skill-b': 60420 },
-	{ 'name': 'Brandon Richards', 'skill-a': 200, 'skill-b': 98787 },
-	{ 'name': 'Samanta Maldonado', 'skill-a': 300, 'skill-b': 716 },
-	{ 'name': 'Faiza Rose', 'skill-a': 400, 'skill-b': 11567 },
-	{ 'name': 'Zephaniah Wang', 'skill-a': 100, 'skill-b': 6426 },
-	{ 'name': 'Willis Partridge', 'skill-a': 101, 'skill-b': 8700 },
-	{ 'name': 'Shannan George', 'skill-a': 300, 'skill-b': 60420 },
-	{ 'name': 'Ishika Dominguez', 'skill-a': 500, 'skill-b': 98787 },
-	{ 'name': 'Thelma Summers', 'skill-a': 123, 'skill-b': 716 },
-	{ 'name': 'Carlie Irving', 'skill-a': 100, 'skill-b': 11567 },
-	{ 'name': 'Lilly-Mai Barber', 'skill-a': 200, 'skill-b': 6426 },
-	{ 'name': 'Osman Tate', 'skill-a': 1, 'skill-b': 8700 },
-	{ 'name': 'Annaliese Coulson', 'skill-a': 3, 'skill-b': 60420 },
-	{ 'name': 'Kaan Mcleod', 'skill-a': 3, 'skill-b': 98787 },
-	{ 'name': 'Constance Ho', 'skill-a': 400, 'skill-b': 716 },
-	{ 'name': 'Saanvi French', 'skill-a': 400, 'skill-b': 11567 },
-	{ 'name': 'Igor Wicks', 'skill-a': 200, 'skill-b': 6426 },
-	{ 'name': 'Scott Devine', 'skill-a': 300, 'skill-b': 8700 },
-	{ 'name': 'Elora King', 'skill-a': 300, 'skill-b': 8700 },
-	{ 'name': 'Mylie Sheehan', 'skill-a': 200, 'skill-b': 8700 },
+	{ 'name': 'Tobias Snyder', 'preference-a': 1, 'preference-b': 100 },
+	{ 'name': 'Brandon Richards', 'preference-a': 2, 'preference-b': 200 },
+	{ 'name': 'Samanta Maldonado', 'preference-a': 3, 'preference-b': 300 },
+	{ 'name': 'Faiza Rose', 'preference-a': 4, 'preference-b': 400 },
+	{ 'name': 'Zephaniah Wang', 'preference-a': 5, 'preference-b': 200 },
+	{ 'name': 'Willis Partridge', 'preference-a': 1, 'preference-b': 8700 },
+	{ 'name': 'Shannan George', 'preference-a': 2, 'preference-b': 60420 },
+	{ 'name': 'Ishika Dominguez', 'preference-a': 3, 'preference-b': 98787 },
+	{ 'name': 'Thelma Summers', 'preference-a': 4, 'preference-b': 716 },
+	{ 'name': 'Carlie Irving', 'preference-a': 5, 'preference-b': 11567 },
+	{ 'name': 'Lilly-Mai Barber', 'preference-a': 1, 'preference-b': 6426 },
+	{ 'name': 'Osman Tate', 'preference-a': 2, 'preference-b': 8700 },
+	{ 'name': 'Annaliese Coulson', 'preference-a': 3, 'preference-b': 60420 },
+	{ 'name': 'Kaan Mcleod', 'preference-a': 3, 'preference-b': 98787 },
+	{ 'name': 'Constance Ho', 'preference-a': 4, 'preference-b': 716 },
+	{ 'name': 'Saanvi French', 'preference-a': 5, 'preference-b': 11567 },
+	{ 'name': 'Igor Wicks', 'preference-a': 1, 'preference-b': 6426 },
+	{ 'name': 'Scott Devine', 'preference-a': 2, 'preference-b': 8700 },
+	{ 'name': 'Elora King', 'preference-a': 3, 'preference-b': 8700 },
+	{ 'name': 'Mylie Sheehan', 'preference-a': 4, 'preference-b': 8700 },
 ];
 
 let vectors = new Array();
 for (let i = 0; i < data.length; i++) {
-	vectors[i] = [data[i]['skill-a'], data[i]['skill-b']];
+	vectors[i] = [data[i]['preference-a'], data[i]['preference-b']];
 }
 
 function FormatGroupsData(groups) {
-	console.log("kmeans raw groups")
+	console.log("kmeans raw groups", groups)
 	let formattedGroups = groups.map(cluster =>
 		cluster.clusterInd.map(index => data[index])
 	);
@@ -50,7 +52,7 @@ const AdminManageGroups = (props) => {
 		if (isSettingGroups) return "";
 		setIsSettingGroups(true);
 
-		kmeans.clusterize(vectors, { k: 5 }, (err, res) => {
+		kmeans.clusterize(vectors, { k: 10 }, (err, res) => {
 			if (err) console.error(err);
 			else {
 				setGroups(FormatGroupsData(res));
@@ -65,7 +67,7 @@ const AdminManageGroups = (props) => {
 
 	return (
 		<div style={{ auto: "max-content" }}>
-			<h1>Manageing groups for {subjectName}</h1>
+			<h1>Managing groups for {subjectName}</h1>
 			<div className={styles.options}>
 				<div className={styles.column}>
 					<button className={styles.btn} disabled={!groups || groups.length == 0} onClick={toggleShowGroups}>View all groups</button>
@@ -78,7 +80,7 @@ const AdminManageGroups = (props) => {
 			<div className="groups" style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
 				{showGroups &&
 					groups.map((group, i) =>
-						<div useRef
+						<div
 							style={{ border: "1px solid black", display: "flex", flexDirection: "column", margin: "30px", padding: "20px", width: "600px", alignItems: "center" }}
 							key={i}>
 							<div style={{ textDecoration: "underline", fontWeight: "bold" }}>Group {i + 1}</div>
