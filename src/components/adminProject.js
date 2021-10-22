ae644b4046486472eb72f85fdf0629d2b365c057
Erina Delinicolas <erinadelinicolas@Erinas-MacBook.local>
2021-10-22 15:52:26 +1100

    changed adminProject from drop down to input field to allow different skill entries for different projects

--- src/components/adminProject.js
+++ src/components/adminProject.js
@@ -9,6 +9,7 @@ import styles from "./adminProject.module.css"
     const [projectName, setProjectName] = React.useState('');
     const [size, setSize] = React.useState('');
     const [skills, setSkills] = React.useState([]);
+    const [skillInput, setSkillInput] = React.useState('');
 
     React.useEffect(()=>{
         console.log(props);
@@ -63,20 +64,16 @@ import styles from "./adminProject.module.css"
                 skills.map((skill, key) => 
                     <div key={key}>
                       <p>{skill}</p>
-                      <button className={styles.removeButton} onClick={()=>{removeSkill(skill)}}>Remove</button>
+                      <button className={styles.smallButton} onClick={()=>{removeSkill(skill)}}>Remove</button>
                     </div>
                 )
             }
             <br/>
             <label>Add Skill</label>
             <br/>
-            <select className={styles.selectSkillMenu} onChange={(ev)=>addSkill(ev.target.value)}>
-                    <option hidden>Select Skill</option>
-                    <option>Skill 1</option>
-                    <option>Skill 2</option>
-                    <option>Skill 3</option>
-                    <option>Skill 4</option>
-            </select> 
+            <input className={styles.input} type="text" onChange={event => setSkillInput(event.target.value)} />
+            <br/>
+            <button className={styles.smallButton} onClick={(ev)=>addSkill(skillInput)}>Add Skill</button>
             <br/>
             <button className={styles.uploadButton} onClick={uploadSurvey}>Upload Survey</button>
          </div>
