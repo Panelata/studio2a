e2c1a78b1a71886ff56925ca2e4094ab55837c9e
codyfp <fisherpeel.c@gmail.com>
2021-10-23 20:49:02 +1100

    stub out Admin Home data for this branch

--- src/components/adminHome.js
+++ src/components/adminHome.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import styles from "./adminHome.module.css"
 import { Link } from 'react-router-dom';
 import axios from "axios";
@@ -9,32 +9,43 @@ class AdminHome extends Component {
 
 	state = {
 		subjects: [],
-		url: "http://127.0.0.1:8000/subject/retrieve"
+		url: "http://127.0.0.1:8000/subject/retrievesadsadasdas"
 	}
-getSubjects = async () => {
-		const subjects = await axios.get(this.state.url);
-		this.setState({subjects: subjects.data});
-	} 
+	getSubjects = async () => {
+		let subjects
+		try {
+			let {data} = await axios.get(this.state.url);
+			subjects = data; 
+		} catch {
+			subjects = [
+				{ subjectID: 1, subjectName: 'SES1a' },
+				{ subjectID: 2, subjectName: 'SES1b' },
+				{ subjectID: 3, subjectName: 'Data Structures' },
+				{ subjectID: 4, subjectName: 'Programming 1' }]
+		}
 
-componentDidMount(){
-	this.getSubjects();
-}
+		this.setState({ subjects });
+	}
+
+	componentDidMount() {
+		this.getSubjects();
+	}
 
 
-render(){
-	return (
-		<div>
+	render() {
+		return (
 			<div>
-			<h1> This is the admin Homepage </h1>
-			</div>
-		<button type="button" className={styles.theButton}>Notifications </button>
+				<div>
+					<h1> This is the admin Homepage </h1>
+				</div>
+				<button type="button" className={styles.theButton}>Notifications </button>
 
-		{(this.state.subjects.length > 0 && <SubjectList subjects={this.state.subjects}/>) 
-		|| (this.state.subjects.length < 1 && <h4>No subjects currently exist, you can create subjects via the "Create a subject" page</h4>)}
-		</div>
-	)
-}
-	
+				{(this.state.subjects.length > 0 && <SubjectList subjects={this.state.subjects} />)
+					|| (this.state.subjects.length < 1 && <h4>No subjects currently exist, you can create subjects via the "Create a subject" page</h4>)}
+			</div>
+		)
+	}
+
 }
 
 
--- src/components/auth.js
+++ src/components/auth.js
@@ -3,45 +3,48 @@
     Create the ability to retain user login state in local storage
 */
 
-class Auth{
-    constructor(){
+class Auth {
+    constructor() {
         this.authenticated = false;
-        this.userType = '';  
+        this.userType = '';
     }
 
     //Handles Login
-    login(login, cb){
-        fetch('http://127.0.0.1:8000/login', {
-            method: 'POST',
-            body: JSON.stringify({
-                username: login[0],
-                password: login[1]
-            })
-        })
-        .then(response =>{
-            return response.json();
-        })
-        .then(data =>{
-            if(data.success){
-                this.authenticated = true;
-                this.userType = data.userType;
-                sessionStorage.setItem('sessionToken', data.token);
-                if(login[2]){
-                    localStorage.setItem('token', data.token);
-                }
-                cb(true);
-            } else {
-                this.authenticated = false;
-                this.userType = '';
-                cb(false);
-               
-            }
-        });
+    login(login, cb) {
+        // fetch('http://127.0.0.1:8000/login', {
+        //     method: 'POST',
+        //     body: JSON.stringify({
+        //         username: login[0],
+        //         password: login[1]
+        //     })
+        // })
+        // .then(response =>{
+        //     return response.json();
+        // })
+        // .then(data =>{
+        //     if(data.success){
+        //         this.authenticated = true;
+        //         this.userType = data.userType;
+        //         sessionStorage.setItem('sessionToken', data.token);
+        //         if(login[2]){
+        //             localStorage.setItem('token', data.token);
+        //         }
+        //         cb(true);
+        //     } else {
+        //         this.authenticated = false;
+        //         this.userType = '';
+        //         cb(false);
+
+        //     }
+        // });
+        this.authenticated = true;
+        this.userType = 'admin'; // change this to student to view the student side
+        cb(true);
     }
 
     //Register new user
-    register(userDetails,cb){
-        fetch('http://127.0.0.1:8000/register',{
+    register(userDetails, cb) {
+        fetch('http://127.0.0.1:8000/register', {
             method: 'POST',
             body: JSON.stringify({
                 firstName: userDetails[0],
@@ -52,19 +55,19 @@ class Auth{
                 userType: userDetails[5]
             })
         })
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            if(data.success){
-                cb(true);
-            } else {
-                cb(false, data.message);
-            }
-        })
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                if (data.success) {
+                    cb(true);
+                } else {
+                    cb(false, data.message);
+                }
+            })
     }
 
-    logout(){
+    logout() {
         localStorage.removeItem('token');
         sessionStorage.removeItem('token');
         this.authenticated = false;
@@ -72,17 +75,17 @@ class Auth{
     }
 
     //Returns user type
-    getUserType(){
+    getUserType() {
         return this.userType;
     }
 
     //Returns authenticated state
-    isAuthenticated(){
+    isAuthenticated() {
         return this.authenticated;
     }
 
     //Token authenticated
-    tokenAuthenticated(userType){
+    tokenAuthenticated(userType) {
         this.authenticated = true;
         this.userType = userType;
     }
