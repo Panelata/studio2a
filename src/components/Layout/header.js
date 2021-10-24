0cb91012dbc4a2625525a77550e122dda9938f75
Hannah van Roy <hannahvanroy@MacBook-Pro-6.local>
2021-10-24 21:17:15 +1100

    removed footer and added background to all pages

--- src/components/Layout/header.js
+++ src/components/Layout/header.js
@@ -1,54 +1,56 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import styles from './header.module.css';
-import auth from '../auth';
-
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "./header.module.css";
+import auth from "../auth";
 
 const renderAdminHeader = () => {
-	return (
-		<div className={styles.headerContainer}>
-			<NavLink to="/admin-home">View all Classes</NavLink>
-			<NavLink to="/admin/register">Register a student</NavLink>
-			<NavLink to="admin/class/create">Create a Subject</NavLink>
-		</div>
-	)
-}
+  return (
+    <div className={styles.headerContainer}>
+      <NavLink to="/admin-home">View all Classes</NavLink>
+      <NavLink to="/admin/register">Register a student</NavLink>
+      <NavLink to="admin/class/create">Create a Subject</NavLink>
+    </div>
+  );
+};
 
 const renderStudentHeader = () => {
-	return (
-		<div className={styles.headerContainer}>
-			<NavLink to="/student-home">View your classes</NavLink>
-		</div>
-	)
-}
+  return (
+    <div className={styles.headerContainer}>
+      <NavLink to="/student-home">View your classes</NavLink>
+    </div>
+  );
+};
 
 const logout = () => {
-	auth.logout();
-	window.location.reload();
-}
+  auth.logout();
+  window.location.reload();
+};
 
 const Header = () => {
-	const [authenticated, setAuthenticated] = React.useState(false);
-	const [userType, setUserType] = React.useState("");
+  const [authenticated, setAuthenticated] = React.useState(false);
+  const [userType, setUserType] = React.useState("");
 
-	React.useEffect(() => {
-		setAuthenticated(auth.isAuthenticated());
-		setUserType(auth.getUserType());
-	}, []);
+  React.useEffect(() => {
+    setAuthenticated(auth.isAuthenticated());
+    setUserType(auth.getUserType());
+  }, []);
 
-	return (
-		<div>
-			{authenticated ?
-				<>
-					You are logged in as a {userType} | <span style={{ cursor: "pointer" }} onClick={logout}>Logout</span>
-					{(userType === "student" && renderStudentHeader()) || (userType === "admin" && renderAdminHeader())}
-				</>
-				:
-				<>
-				</>
-			}
-		</div>
-	)
-}
+  return (
+    <div>
+      {authenticated ? (
+        <>
+          You are logged in as a {userType} |{" "}
+          <span style={{ cursor: "pointer" }} onClick={logout}>
+            Logout
+          </span>
+          {(userType === "student" && renderStudentHeader()) ||
+            (userType === "admin" && renderAdminHeader())}
+        </>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+};
 
 export default Header;
