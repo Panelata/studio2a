ae794854e8a5fc20daab59b9b1f0e193512d8449
codyfp <fisherpeel.c@gmail.com>
2021-10-24 18:41:47 +1100

    Small update to merge survey responses util (wip)

--- src/Utils/MergeSurveyResponses.js
+++ src/Utils/MergeSurveyResponses.js
@@ -1,10 +1,16 @@
 
 
 export default function (data) {
+	
+
+	
 	let mergedData = data.forEach(row => {
 		data.forEach(user => {
+			// console.log(user)
 			data.forEach(row => {
-				
+				if (user.userID == row.userID && user.mappingID != row.mappingID){
+					
+				}
 			})
 		})
 	});
