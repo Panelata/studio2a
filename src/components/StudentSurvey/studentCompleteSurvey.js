56d823233fba04d312f87eeda7cef67a36e2dc60
codyfp <fisherpeel.c@gmail.com>
2021-10-25 12:37:44 +1100

    adding card to manage groups pages and student survey page

--- src/components/StudentSurvey/studentCompleteSurvey.js
+++ src/components/StudentSurvey/studentCompleteSurvey.js
@@ -1,10 +1,11 @@
 //To save time lets just hard code the skills
 
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { useHistory } from 'react-router-dom';
 import axios from "axios";
 import { Link } from 'react-router-dom';
 import SkillList from './SkillList';
+import Card from "@mui/material/Card";
 
 class studentCompleteSurvey extends Component {
 
@@ -14,25 +15,43 @@ class studentCompleteSurvey extends Component {
 	}
 	getSkillsMappings = async () => {
 		const skills = await axios.get(this.state.url);
-		this.setState({skills: skills.data});
+		this.setState({ skills: skills.data });
 		console.log(skills);
-	} 
+	}
 
-componentDidMount(){
-	this.getSkillsMappings();
+	componentDidMount() {
+		this.getSkillsMappings();
+	}
+
+	render() {
+		return (
+			<div
+				style={{
+					display: "flex",
+					justifyContent: "center",
+					// alignItems: "center",
+					height: "100vh",
+				}}
+			>
+				<Card
+					sx={{
+						width: "90vh",
+						height: "100vh",
+						paddingTop: 5,
+						paddingBottom: 5,
+					}}
+					className="card"
+				>
+
+					<h1 style={{ marginBottom: "20px" }}> Completing Survey for Project: {this.props.location.projectName}</h1>
+					<h4 style={{ marginBottom: "20px" }}>Enter a confidence score from 1-10 next to each skill</h4>
+					{(this.state.skills.length > 0 && <SkillList numtoshow={1} skills={this.state.skills} />)
+						|| (this.state.skills.length < 1 && <h4> No Skills available</h4>)}
+					{/* <h4>Note: Need to write code to send survey data </h4> */}
+				</Card>
+			</div>
+		)
+	}
 }
 
-    render(){
-        return (
-			<div>
-				<h1> Completing Survey for Project: {this.props.location.projectName}</h1>
-				<h4>please enter a confidence score from 1-10 next to each skill</h4>
-				{(this.state.skills.length > 0 && <SkillList numtoshow={1} skills={this.state.skills}/>)
-		|| (this.state.skills.length < 1 && <h4> No Skills available</h4>)}
-				<h4>Note: Need to write code to send survey data </h4>
-			</div>
-        )
-    }
- }
-
- export default studentCompleteSurvey;
+export default studentCompleteSurvey;
--- src/components/studentProject.js
+++ src/components/studentProject.js
@@ -4,6 +4,7 @@ import React, { useState, useRef } from 'react';
 import { useHistory } from 'react-router-dom';
 import styles from "./studentProject.module.css"
 import { Link } from 'react-router-dom';
+import Card from "@mui/material/Card";
 import StudentShowGroups from './studentShowGroups';
 
 const StudentProject = (props) => {
@@ -20,25 +21,42 @@ const StudentProject = (props) => {
 	}, [viewProjects]);
 
 	return (
-		<div>
-			<h1>Viewing Project: {props.location.projectName}</h1>
-			<div className={styles.options}>
-				<div className={styles.column}>
-					{/* TODO: flip disabled prop for enrolled group once backend is setup (should be disable if a group doesn't exist) */}
-					<button className={styles.btn} >View enrolled group</button>
-					<button className={styles.btn} onClick={toggleViewProjects}>View all groups</button>
-				</div>
-				<div className={styles.column}>
-					<div className={styles.btn}>
-						<Link className={styles.link} to={{
-							pathname: "/student/class/project/completeSurvey",
-							projectID: props.location.projectID,
-							projectName: props.location.projectName,
-						}}>Take survey to assign yourself to a group</Link>
+		<div
+			style={{
+				display: "flex",
+				justifyContent: "center",
+				// alignItems: "center",
+				height: "100vh",
+			}}
+		>
+			<Card
+				sx={{
+					width: "90vh",
+					height: "100vh",
+					paddingTop: 5,
+					paddingBottom: 5,
+				}}
+				className="card"
+			>
+				<h1>Viewing Project: {props.location.projectName}</h1>
+				<div className={styles.options}>
+					<div className={styles.column}>
+						{/* TODO: flip disabled prop for enrolled group once backend is setup (should be disable if a group doesn't exist) */}
+						<button className={styles.btn} >View enrolled group</button>
+						<button className={styles.btn} onClick={toggleViewProjects}>View all groups</button>
+					</div>
+					<div className={styles.column}>
+						<div className={styles.btn} style={{width: "auto"}}>
+							<Link className={styles.link} to={{
+								pathname: "/student/class/project/completeSurvey",
+								projectID: props.location.projectID,
+								projectName: props.location.projectName,
+							}}>Take survey to assign yourself to a group</Link>
+						</div>
 					</div>
 				</div>
-			</div>
-			{viewProjects && <div ref={groupsRef}><StudentShowGroups projectID={props.location.projectID} /></div>}
+				{viewProjects && <div ref={groupsRef}><StudentShowGroups projectID={props.location.projectID} /></div>}
+			</Card>
 		</div>
 	)
 }
